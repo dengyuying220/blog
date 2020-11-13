b7a2cbd11750c884e11f35818e6bad572b01d0c1
Your Name <you@example.com>
2020-11-13 19:34:33 +0800

    template

--- blog/src/main/java/com/deng/blog/web/IndexController.java
+++ blog/src/main/java/com/deng/blog/web/IndexController.java
@@ -10,14 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
  **/
 @Controller
 public class IndexController {
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id, @PathVariable String name) {
+    @GetMapping("/")
+    public String index(/*@PathVariable Integer id, @PathVariable String name*/) {
 //        int i = 9/0;
 //        String blog = null;
 //        if (blog == null) {
 //            throw  new NotFoundException("博客不存在");
 //        }
         System.out.println("--------------index--------------");
-        return "index";
+        return "blog";
     }
 }
