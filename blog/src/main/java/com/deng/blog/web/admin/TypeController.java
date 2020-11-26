caf62308b06b760deedc60b0bd48897edb4477a1
Your Name <you@example.com>
2020-11-26 18:15:48 +0800

    type-2

--- blog/src/main/java/com/deng/blog/web/admin/TypeController.java
+++ blog/src/main/java/com/deng/blog/web/admin/TypeController.java
@@ -1,5 +1,6 @@
 package com.deng.blog.web.admin;
 
+import com.deng.blog.po.Type;
 import com.deng.blog.service.TypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -23,10 +25,24 @@ public class TypeController {
     private TypeService typeService;
 
     @GetMapping("/type")
-    public String list(@PageableDefault(size = 10, sort = {"id"}, direction = Sort.Direction.DESC)
+    public String list(@PageableDefault(size = 2, sort = {"id"}, direction = Sort.Direction.DESC)
                                    Pageable pageable, Model model) {
         model.addAttribute("page", typeService.listType(pageable));
         return "/admin/type";
+    }
+
+    @GetMapping("/type/input")
+    public String input() {
+        return "admin/type-input";
+    }
+
+    @PostMapping("/type")
+    public String post(Type type) {
+        Type t = typeService.saveType(type);
+        if (t == null) {
+//
+        }
+        return "redirect:/admin/type";
     }
 
 }
